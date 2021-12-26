aae7b5fbf0d76b8e8207b3bbbb79f89642d49a05
Michael <michaeldobin@me.com>
2021-12-26 16:42:59 -0600

    Initial Commit

---
+++ GHlearningm3l2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GHlearningm3l2
+//
+//  Created by Michael Dobin on 12/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GHlearningm3l2/GHlearningm3l2App.swift
@@ -0,0 +1,17 @@
+//
+//  GHlearningm3l2App.swift
+//  GHlearningm3l2
+//
+//  Created by Michael Dobin on 12/23/21.
+//
+
+import SwiftUI
+
+@main
+struct GHlearningm3l2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
